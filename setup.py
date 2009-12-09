58ed7c96de92662afd7878a1692e093298d036fc
Zachary Voase <zacharyvoase@me.com>
2009-12-09 23:18:52 +0100

    Added setup.py and ez_setup.py files.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+from ez_setup import use_setuptools; use_setuptools()
+from setuptools import setup, find_packages
+
+
+rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
+
+def read_from(filename):
+    fp = open(filename)
+    try:
+        return fp.read()
+    finally:
+        fp.close()
+
+def get_version():
+    data = read_from(rel_file('src', 'djboss', '__init__.py'))
+    return re.search(r"__version__ = '([^']+)'", data).group(1)
+
+def get_requirements():
+    data = read_from(rel_file('REQUIREMENTS'))
+    lines = map(lambda s: s.strip(), data.splitlines())
+    return filter(None, lines)
+
+
+setup(
+    name             = 'django-boss',
+    version          = get_version(),
+    author           = "Zachary Voase",
+    author_email     = "zacharyvoase@me.com",
+    url              = 'http://bitbucket.org/zacharyvoase/django-boss',
+    description      = "Django management commands, revisited.",
+    packages         = find_packages(where='src'),
+    package_dir      = {'': 'src'},
+    install_requires = get_requirements(),
+)
