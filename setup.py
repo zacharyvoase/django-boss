3b0fb637fc4e753524deca82b93878d7c3afad5a
Zachary Voase <zacharyvoase@me.com>
2009-12-14 18:36:44 +0100

    Switched from setuptools to distribute.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 import re
 
-from ez_setup import use_setuptools; use_setuptools()
+from distribute_setup import use_setuptools; use_setuptools()
 from setuptools import setup, find_packages
 
 
